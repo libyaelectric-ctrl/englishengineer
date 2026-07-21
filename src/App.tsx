220a3867f06d3733286690380f6f5a9f409fcc9d
libyaelectric-ctrl <libyaelectric@gmail.com>
2026-07-21 19:46:51 +0200

    revert(mascot): remove framer-motion mascot entirely, restore original state

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,11 @@ import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@/routes/router';
 import { AppProvider } from '@/providers';
-import { Mascot } from '@/components/Mascot';
 
 export const App: React.FC = () => {
   return (
     <AppProvider>
       <RouterProvider router={router} />
-      <div className="fixed bottom-4 right-4 z-50">
-        <Mascot size={80} />
-      </div>
     </AppProvider>
   );
 };
