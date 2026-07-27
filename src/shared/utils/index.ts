ad6b8d7307e651f2a076e35d830537e4db8b6f2d
libyaelectric-ctrl <libyaelectric@gmail.com>
2026-07-28 00:36:02 +0200

    fix(utils): add index.ts barrel export for shared utils

---
+++ src/shared/utils/index.ts
@@ -0,0 +1,5 @@
+export { cn } from './cn';
+export { sanitize } from './sanitize';
+export { prefetch } from './prefetch';
+export { sound } from './sound';
+export { indexedDb } from './indexed-db';
