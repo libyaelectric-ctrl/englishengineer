55c10ce59b5ac9396fc15e5c72e9485625be2409
libyaelectric-ctrl <libyaelectric@gmail.com>
2026-08-22 20:27:21 +0200

    fix(login): Clerk config paths match router + test mock ClerkProvider

---
+++ src/features/mascot/index.ts
@@ -0,0 +1,4 @@
+export { EngMascot } from './EngMascot';
+export { useMascotStore } from './mascot.store';
+export { useMascotEvents } from './useMascotEvents';
+export type { MascotState } from './mascot.store';
